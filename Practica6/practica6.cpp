0fb1b4a5e6142312211c8c8081a19b6406a92627
unknown <pitu96_drn23@hotmail.com>
2017-11-24 09:38:38 +0100

    velocidad fluida y dibujo de carretera a partir de tu posicion

--- Practica6/practica6.cpp
+++ Practica6/practica6.cpp
@@ -11,10 +11,10 @@
 #include <Utilidades.h>
 
 using namespace std;
-static int tasaFPS = 30;
-static int posx = 0, y = 1, z = 0;
+static float posx = 0, y = 1, posz = 0;
 static float velx = 0.0;
 static float giroy = 0.0;
+static float quadSize = 20;
 
 void init() {
 
@@ -38,11 +38,16 @@ void display() {
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	gluLookAt(posx * cos(giroy * PI / 180), 1, posx * sin(giroy * PI /180), 10000 * cos(giroy * PI / 180), 0, 10000 * sin(giroy * PI /180), 0, 1, 0);
+	gluLookAt(posx, 1, posz, 10000 * cos(giroy), 0, 10000 * sin(giroy), 0, 1, 0);
+
 	glColor3f(0, 0, 0);
 	glPolygonMode(GL_FRONT, GL_LINE);
-	for (int i = 0; i < 20; i++) {
-		GLfloat v0[3] = { 20 * i,0, 5 }, v1[3] = { 20 * (i + 1),0,5 }, v2[3] = { 20 * (i + 1),0,-5 }, v3[3] = { 20 * i,0,-5 };
+	// Dibujar la carretera a partir de tu posicion
+	for (int i = posx/quadSize; i < 50 + posx; i++) {
+		GLfloat v0[3] = { quadSize*i,0, 5 };
+		GLfloat v1[3] = { quadSize*(i + 1),0,5 };
+		GLfloat v2[3] = { quadSize*(i + 1),0,-5 };
+		GLfloat v3[3] = { quadSize*i,0,-5 };
 		quad(v0, v1, v2, v3, 10, 5);
 	}
 	
@@ -72,10 +77,10 @@ void onKey(int tecla, int x, int y) {
 		if (velx < 0) velx = 0.0;
 		break;
 	case GLUT_KEY_LEFT:
-		giroy -= 0.25;
+		giroy -= 0.25 * PI/ 180;
 		break;
 	case GLUT_KEY_RIGHT:
-		giroy += 0.25;
+		giroy += 0.25 * PI /180;
 		break;
 	}
 	glutPostRedisplay();
@@ -85,8 +90,11 @@ void update() {
 	static int hora_anterior = glutGet(GLUT_ELAPSED_TIME);
 	// Velocidad de la animacion (cte)
 	int hora_actual = glutGet(GLUT_ELAPSED_TIME);
-	float tiempo_transcurrido = (hora_actual - hora_anterior) / 1000.0f;  // sg
-	posx = velx * tiempo_transcurrido;
+	float tiempo_transcurrido = (hora_actual - hora_anterior) / 1000.0f;
+	posx += velx * cos(giroy) * tiempo_transcurrido;
+	posz += velx * sin(giroy) * tiempo_transcurrido;
+
+	hora_anterior = hora_actual;
 	glutPostRedisplay();
 }
 
@@ -105,7 +113,8 @@ void main(int argc, char** argv) {
 	glutCreateWindow(PROYECTO);
 	init();
 	glutDisplayFunc(display);
-	glutTimerFunc(1000 / tasaFPS, updateM, 1000 / tasaFPS);
+	glutIdleFunc(update);
+	//glutTimerFunc(1000 / tasaFPS, updateM, 1000 / tasaFPS);
 	glutReshapeFunc(resize);
 	glutSpecialFunc(onKey);
 	glutMainLoop();
